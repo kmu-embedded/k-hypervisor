1143f0716d4933fa7b31ea52baf628924ce2c2a3
iamwonseok <wonseok.ko@outlook.com>
2015-11-15 22:11:17 +0900

    Removed stdio library from freeBSD.         - fixed debugging issues in maxview

--- arch/arm/sscanf.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SSCANF_H__
-#define __SSCANF_H__
-
-#include <stddef.h>
-#include <stdarg.h>
-
-int sscanf(const char *s, const char *fmt, ...);
-#endif
-
--- config.mk
+++ config.mk
@@ -1,11 +1,10 @@
-# TODO: This file will be generated automatically.
 # Author has to know the difference among '?=', '=', '+=' and ':='
-#
+
 CROSS_COMPILE		?= arm-linux-gnueabihf-
 PROCESSOR			?= cortex-a15
 
 DEBUG_FLAG			= -g
-CPPFLAGS			= -mcpu=$(PROCESSOR) -marm $(DEBUG_FLAGS)
+CONFIG_FLAG			= -mcpu=$(PROCESSOR) -marm
 
 MACH				?= rtsm
 PROJECT				?= khypervisor
