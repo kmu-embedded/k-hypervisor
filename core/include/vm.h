3362d1a3de834ecd14a69761a87f255e0f24a8bb
casionwoo <casionwoo@gmail.com>
2015-11-23 19:19:48 +0900

    Added vCPU structure and APIs

--- core/include/vm.h
+++ core/include/vm.h
@@ -1,15 +1,15 @@
 #ifndef __VM_H__
 #define __VM_H__
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <list.h>
+#include <vcpu.h>
+#include <hvmm_types.h>
 
 #define SIZE_OF_NAME        32
-#define VM_CREATE_FAILED    255
-#define VM_NOT_EXISTED      254
+#define NO_VM_FOUND         NULL
+#define VM_NOT_EXISTED      -1
+#define VM_CREATE_FAILED    -2
 
-typedef unsigned char vmid_t;
 typedef enum vmcb_state {
     UNDEFINED,
     DEFINED,
@@ -23,7 +23,7 @@ struct vmcb {
     char name[SIZE_OF_NAME];
     unsigned char num_vcpus;
 
-    // TODO(casionwoo) : Define vCPU
+    struct vcpu **vcpu;
     // TODO(casionwoo) : Define vMEM
     // TODO(casionwoo) : Define vIRQ
 
@@ -31,11 +31,11 @@ struct vmcb {
     struct list_head head;
 };
 
-int vm_setup();
+hvmm_status_t vm_setup();
 vmid_t vm_create(unsigned char num_vcpu);
-vmcb_state_t vm_delete(vmid_t vmid);
 vmcb_state_t vm_init(vmid_t vmid);
 vmcb_state_t vm_start(vmid_t vmid);
+vmcb_state_t vm_delete(vmid_t vmid);
 
 // TODO(casionwoo) : vm_suspend, resume, shutdown
 // TODO(casionwoo) : vm_save, vm_restore
@@ -43,5 +43,5 @@ vmcb_state_t vm_start(vmid_t vmid);
 struct vmcb *vm_find(vmid_t vmid);
 void print_all_vm();
 
-#endif /*__VM_H__*/
+#endif /* __VM_H__ */
 
---
+++ core/vm/test_vm.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <vm.h>
+
+#define VM_TOTAL_NUM      10
+#define VM_NUM            5
+#define VM_VCPU_NUM       2
+
+void TEST_VMCB();
+
+signed char vmcb[VM_TOTAL_NUM] = {0, };
+
+void TEST_VMCB()
+{
+    int i = 0;
+
+    printf("============%s=================\n", __func__);
+    for(i = 0; i < VM_NUM; i++) {
+        if ((vmcb[i] = vm_create(VM_VCPU_NUM)) == VM_CREATE_FAILED) {
+            printf("%dth vm creat fail\n", i);
+        }
+
+        if (vm_init(vmcb[i]) != HALTED) {
+            printf("%dth vm init fail\n", i);
+        }
+
+        if (vm_start(vmcb[i]) != RUNNING) {
+            printf("%dth vm start fail\n", i);
+        }
+    }
+    print_all_vm();
+    printf("====%d vm delete\n", 5);
+    if (vm_delete(5) != UNDEFINED) {
+        printf("ID : 5 vm delete failed\n");
+    }
+    print_all_vm();
+    printf("====%d vm delete\n", vmcb[0]);
+    if (vm_delete(vmcb[0]) != UNDEFINED) {
+        printf("ID : 0 vm delete failed\n");
+    }
+    print_all_vm();
+    printf("====%d vm delete\n", vmcb[3]);
+    if (vm_delete(vmcb[3]) !=  UNDEFINED) {
+        printf("ID : 3 vm delete failed\n");
+    }
+    print_all_vm();
+    printf("====%d vm delete\n", vmcb[2]);
+    if (vm_delete(vmcb[2]) != UNDEFINED) {
+        printf("ID : 2 vm delete failed\n");
+    }
+    print_all_vm();
+    printf("====%d vm delete\n", vmcb[1]);
+    if (vm_delete(vmcb[1]) != UNDEFINED) {
+        printf("ID : 1 vm delete failed\n");
+    }
+    print_all_vm();
+    printf("====%d vm delete\n", vmcb[4]);
+    if (vm_delete(vmcb[4]) != UNDEFINED) {
+        printf("ID : 4 vm delete failed\n");
+    }
+    print_all_vm();
+}
+
