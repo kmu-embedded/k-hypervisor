a2e98597413d83f2833c6b508e2bdbe3a05cea0a
casionwoo <casionwoo@gmail.com>
2015-12-09 09:48:04 +0900

    Refactored vCPU as my upstream vCPU

--- tests/tests_gic_timer.c
+++ tests/tests_gic_timer.c
@@ -43,7 +43,8 @@ void interrupt_nsptimer(int irq, void *pregs, void *pdata)
     /* Test guest context switch */
     if ((regs->cpsr & 0x1F) != 0x1A) {
         /* Not from Hyp, switch the guest context */
-        vcpu_dump_regs(regs);
+//        vcpu_dump_regs(regs);
+        vcpu_regs_dump(GUEST_VERBOSE_ALL, regs);
         guest_switchto(sched_policy_determ_next(), 0);
     }
     HVMM_TRACE_EXIT();
