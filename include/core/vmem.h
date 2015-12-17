b5958312a4dd4fe0a03afd64efc03b01aec14a4c
casionwoo <casionwoo@gmail.com>
2015-12-17 23:43:03 +0900

    Added a vmem structure into vmcb structure

--- include/core/vmem.h
+++ include/core/vmem.h
@@ -4,10 +4,19 @@
 #include <hvmm_types.h>
 #include <stdint.h>
 #include <guest_memory_hw.h>
+#include <arch/arm/rtsm-config.h>
 
-void memory_create();
-hvmm_status_t memory_init(struct memmap_desc **guest0, struct memmap_desc **guest1);
-hvmm_status_t memory_save(void);
-hvmm_status_t memory_restore(vmid_t vmid);
+// TODO(casionwoo) : This declaration will be moved to hvmm_types.h
+typedef uint32_t uint32ptr_t;
+
+struct vmem {
+    struct memmap_desc **memmap;
+    uint32ptr_t pgtable_base;
+};
+
+void vmem_create();
+hvmm_status_t vmem_init(struct vmem *vmem, vmid_t vmid);
+hvmm_status_t vmem_save(void);
+hvmm_status_t vmem_restore(struct vmem *vmem, vmid_t vmid);
 
 #endif
