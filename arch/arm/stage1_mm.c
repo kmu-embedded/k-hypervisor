20cc0cbd7e0ae0205594e836d23eafdd21601548
casionwoo <casionwoo@gmail.com>
2016-01-08 22:51:49 +0900

    Modified definition in stage1_mm.c

--- arch/arm/stage1_mm.c
+++ arch/arm/stage1_mm.c
@@ -82,15 +82,15 @@ static pgentry set_entry(uint32_t paddr, uint8_t attr_indx, pgsize_t size)
     return entry;
 }
 
-#define HSCTLR_TE        (1<<30) /**< Thumb Exception enable. */
-#define HSCTLR_EE        (1<<25) /**< Exception Endianness. */
-#define HSCTLR_FI        (1<<21) /**< Fast Interrupts configuration enable. */
-#define HSCTLR_WXN       (1<<19) /**< Write permission emplies XN. */
-#define HSCTLR_I         (1<<12) /**< Instruction cache enable.  */
-#define HSCTLR_CP15BEN   (1<<7)  /**< In ARMv7 this bit is RAZ/SBZP. */
-#define HSCTLR_C         (1<<2)  /**< Cache enable. */
-#define HSCTLR_A         (1<<1)  /**< Alignment check enable. */
-#define HSCTLR_M         (1<<0)  /**< MMU enable. */
+#define HSCTLR_TE        (1 << 30) /**< Thumb Exception enable. */
+#define HSCTLR_EE        (1 << 25) /**< Exception Endianness. */
+#define HSCTLR_FI        (1 << 21) /**< Fast Interrupts configuration enable. */
+#define HSCTLR_WXN       (1 << 19) /**< Write permission emplies XN. */
+#define HSCTLR_I         (1 << 12) /**< Instruction cache enable.  */
+#define HSCTLR_CP15BEN   (1 << 7)  /**< In ARMv7 this bit is RAZ/SBZP. */
+#define HSCTLR_C         (1 << 2)  /**< Cache enable. */
+#define HSCTLR_A         (1 << 1)  /**< Alignment check enable. */
+#define HSCTLR_M         (1 << 0)  /**< MMU enable. */
 #define HSCTLR_BASE       0x30c51878  /**< HSTCLR Base address */
 
 #define OUTER_WRITETHROUGH_CACHEABLE (WRITETHROUGH_CACHEABLE << 10)
