19031b86f57ba8e2f33add20dc4341072ce58da4
casionwoo <casionwoo@gmail.com>
2016-01-08 14:04:56 +0900

    Moved common definition used in stage[12]_mm.c from stage2_mm.h to mm.h

--- arch/arm/stage2_mm.h
+++ arch/arm/stage2_mm.h
@@ -3,14 +3,6 @@
 
 #include <vmem.h>
 
-#define LPAE_PAGE_SHIFT    12
-#define LPAE_PAGE_SIZE      (1<<LPAE_PAGE_SHIFT)
-#define LPAE_PAGE_MASK      (0xFFF)
-
-#define LPAE_BLOCK_L2_SHIFT 21
-#define LPAE_BLOCK_L2_SIZE  (1<<LPAE_BLOCK_L2_SHIFT)
-#define LPAE_BLOCK_L2_MASK  (0x1FFFFF)
-
 union stage2_pgentry
 {
     uint64_t raw;
