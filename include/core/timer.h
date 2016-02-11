b8022e184c9dc641f5ab6e8b2db7a1a9177affc3
iamwonseok <wonseok.ko@outlook.com>
2016-02-11 15:01:25 +0900

    Added checking routine for generic timer

--- include/core/timer.h
+++ include/core/timer.h
@@ -16,50 +16,19 @@ struct timer_val {
 };
 
 struct timer_ops {
-    /** The init function should only be used in the entire system */
     hvmm_status_t (*init)(void);
-
-    /** Enable the timer interrupt */
     hvmm_status_t (*enable)(void);
-
-    /** Disable the timer interrupt */
     hvmm_status_t (*disable)(void);
-
-    /** Set timer duration */
     hvmm_status_t (*set_interval)(uint64_t);
-
-    /** Dump state of the timer */
     hvmm_status_t (*dump)(void);
-
 };
 
 struct timer_module {
-    /** tag must be initialized to HAL_TAG */
-    uint32_t tag;
-
-    /**
-     * Version of the module-specific device API. This value is used by
-     * the derived-module user to manage different device implementations.
-     * The user who uses this module is responsible for checking
-     * the module_api_version and device version fields to ensure that
-     * the user is capable of communicating with the specific module
-     * implementation.
-     *
-     */
     uint32_t version;
-
-    /** Identifier of module */
     const char *id;
-
-    /** Name of this module */
     const char *name;
-
-    /** Author/owner/implementor of the module */
     const char *author;
-
-    /** Timer Operation */
     struct timer_ops *ops;
-
 };
 
 extern struct timer_module _timer_module;
@@ -70,13 +39,11 @@ extern struct timer_module _timer_module;
 hvmm_status_t timer_init(uint32_t irq);
 hvmm_status_t timer_set(struct timer_val *timer, uint32_t host);
 
-
 void set_timer_cnt(void);
 uint64_t get_timer_savecnt(void);
 uint64_t get_timer_curcnt(void);
 uint64_t get_timer_cnt(void);
 uint32_t get_timer_interval_us(uint64_t after, uint64_t before);
-
 
 hvmm_status_t timer_stop(void);
 hvmm_status_t timer_start(void);
