356b777f5356ad57a4ec650c5fdc98ce2daa5c0d
iamwonseok <wonseok.ko@outlook.com>
2016-02-12 19:05:27 +0900

    Added  in config.mk

--- config.mk
+++ config.mk
@@ -1,25 +1,54 @@
 # Author has to know the difference among '?=', '=', '+=' and ':='
 
-CROSS_COMPILE		?= arm-linux-gnueabihf-
-PROCESSOR		?= cortex-a15
+CROSS_COMPILE	= arm-linux-gnueabihf-
+PROCESSOR		= cortex-a15
 
 DEBUG_FLAG		= -g
 CONFIG_FLAG		= -mcpu=$(PROCESSOR) -marm
 
-MACHINE			?= rtsm
-PROJECT			?= khypervisor
-TARGET			?= khypervisor-$(MACHINE)
+MACHINE			= rtsm
+PROJECT			= khypervisor
+TARGET			= khypervisor-$(MACHINE)
 
 # Desciption of directory structure
-ROOT			?= .
-ARCH			?= arch/arm
-VERSION			?= v7
-CORE			?= core
-TESTS			?= tests
-LIB			?= lib
-DRV			?= drivers
-INC			?= include
-MKDIR			?= mkdir
-BUILD			?= build
+ROOT			= $(PWD)
+ARCH			= arch/arm
+VERSION			= v7
+CORE			= core
+TESTS			= tests
+LIB				= lib
+DRV				= drivers
+INC				= include
+MKDIR			= mkdir
+BUILD			= build
+MAKE			= make
 
-MAKE			?= make
+INCLUDES		=  -I $(ROOT)/
+INCLUDES		+= -I $(ROOT)/$(INC)
+INCLUDES		+= -I $(ROOT)/$(INC)/board
+INCLUDES		+= -I $(ROOT)/$(INC)/$(LIB)/c
+INCLUDES		+= -I $(ROOT)/$(INC)/$(LIB)/bsd
+INCLUDES		+= -I $(ROOT)/$(INC)/$(LIB)
+INCLUDES		+= -I $(ROOT)/$(INC)/$(CORE)
+INCLUDES		+= -I $(ROOT)/$(INC)/$(DRV)
+INCLUDES		+= -I $(ROOT)/$(INC)/$(ARCH)/$(ARM)
+INCLUDES		+= -I $(ROOT)/$(ARCH)/$(ARM)
+
+INCLUDES		+= -I $(ROOT)/$(CORE)/libhw
+INCLUDES		+= -I $(ROOT)/$(CORE)
+INCLUDES		+= -I $(ROOT)/$(CORE)/$(INC)
+
+INCLUDES		+= -I $(ROOT)/$(LIB)
+
+INCLUDES		+= -I $(ROOT)/$(TESTS)
+INCLUDES		+= -I $(ROOT)/$(TESTS)/libs
+
+CC			= $(CROSS_COMPILE)gcc
+LD			= $(CROSS_COMPILE)ld
+NM			= $(CROSS_COMPILE)nm
+OBJCOPY		= $(CROSS_COMPILE)objcopy
+
+
+CPPFLAGS	= $(CONFIG_FLAG) $(INCLUDES) -ffreestanding -nostdlib -nodefaultlibs -nostartfiles $(DEBUG_FLAG)
+CPPFLAGS	+= -Wall
+
