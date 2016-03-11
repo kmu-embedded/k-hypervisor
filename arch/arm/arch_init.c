0782891e2a84cfad500bee302d330a589a2c6630
casionwoo <casionwoo@gmail.com>
2016-03-12 01:25:07 +0900

    Added prefix 'arch_' to  functions in arch_init.[ch]

--- arch/arm/arch_init.c
+++ arch/arm/arch_init.c
@@ -2,7 +2,7 @@
 #include "mm.h"
 
 extern uint32_t __hvc_vector;
-void SECTION(".init.arch") cpu_init()
+void SECTION(".init.arch") arch_cpu_init()
 {
     uint32_t vector_base = (uint32_t) &__hvc_vector;
     write_hvbar(vector_base);
@@ -13,7 +13,7 @@ void SECTION(".init.arch") cpu_init()
 
 #include "gic-v2.h"
 #include "vgic.h"
-void SECTION(".init.arch") irq_init()
+void SECTION(".init.arch") arch_irq_init()
 {
     // TODO(casionwoo): add a init function of irq handler table for hypervisor.
     gic_init();
