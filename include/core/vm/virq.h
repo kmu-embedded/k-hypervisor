b533dcc94b0e4661bbf81fdc49d231c62f4db006
casionwoo <casionwoo@gmail.com>
2016-03-11 23:15:41 +0900

    Moved struct guest_virqmap from interrupt.h to virq.h

--- include/core/vm/virq.h
+++ include/core/vm/virq.h
@@ -7,6 +7,24 @@
 #include "../../../arch/arm/vgic.h"
 #include <core/interrupt.h>
 
+/**
+ * @breif   Saves a mapping information to find a virq for injection.
+ *
+ * We do not consider a sharing device that's why we save only one vmid.
+ * @todo    (wonseok): need to change a structure when we support
+ *                     a sharing device among guests.
+ */
+struct virqmap_entry {
+    uint32_t enabled;   /**< virqmap enabled flag */
+    uint32_t virq;      /**< Virtual interrupt nubmer */
+    uint32_t pirq;      /**< Pysical interrupt nubmer */
+};
+
+struct guest_virqmap {
+    vmid_t vmid;
+    struct virqmap_entry map[1024];
+};
+
 struct virq {
     struct vgic_status vgic_status;
     struct guest_virqmap *guest_virqmap;
