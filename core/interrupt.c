84cc5651f7e1c88224da3b0e258d03a0ba88d1fa
casionwoo <casionwoo@gmail.com>
2016-03-14 16:26:20 +0900

    Modified the routine of interrupt_service_routine()

--- core/interrupt.c
+++ core/interrupt.c
@@ -22,26 +22,8 @@
 /**< IRQ handler */
 static interrupt_handler_t interrupt_handlers[MAX_IRQS];
 
-static uint32_t interrupt_check_guest_irq(uint32_t pirq)
-{
-    uint32_t pcpu = smp_processor_id();
-    struct virqmap_entry *map;
-    struct running_vcpus_entry_t *sched_vcpu_entry;
-    struct vcpu *vcpu;
-
-    list_for_each_entry(struct running_vcpus_entry_t, sched_vcpu_entry, &__running_vcpus[pcpu], head) {
-        vcpu = vcpu_find(sched_vcpu_entry->vcpuid);
-        map = vcpu->virq.guest_virqmap->map;
-        if (map[pirq].virq != VIRQ_INVALID) {
-            return GUEST_IRQ;
-        }
-    }
-
-    return HOST_IRQ;
-}
-
 #define __UART_IRQ_DEBUG__
-static void interrupt_inject_enabled_guest(int num_of_guests, uint32_t irq)
+static void interrupt_inject_enabled_guest(uint32_t irq)
 {
     uint32_t virq;
     struct vcpu *vcpu;
@@ -56,7 +38,7 @@ static void interrupt_inject_enabled_guest(int num_of_guests, uint32_t irq)
         }
 #ifdef __UART_IRQ_DEBUG__
         if (irq != 34) {
-            printf("vmid %d: pirq %d virq %d\n", vcpu->vcpuid, irq, virq);
+            printf("vcpuid %d: pirq %d virq %d\n", vcpu->vcpuid, irq, virq);
         }
 #endif
         virq_inject(vcpu->vcpuid, virq, irq, INJECT_HW);
@@ -88,18 +70,12 @@ void interrupt_service_routine(int irq, void *current_regs)
 
     struct arch_regs *regs = (struct arch_regs *)current_regs;
 
-    if (interrupt_check_guest_irq(irq) == GUEST_IRQ) {
+    gic_completion_irq(irq);
+    interrupt_inject_enabled_guest(irq);
 
-        /* IRQ INJECTION */
-        /* priority drop only for hanlding irq in guest */
-        gic_completion_irq(irq);
-        interrupt_inject_enabled_guest(NUM_GUESTS_STATIC, irq);
-    } else {
-        /* host irq */
-        if (interrupt_handlers[irq])
-            interrupt_handlers[irq](irq, regs, 0);
-
-        gic_completion_irq(irq);
+    /* host irq */
+    if (interrupt_handlers[irq]) {
+        interrupt_handlers[irq](irq, regs, 0);
         gic_deactivate_irq(irq);
     }
 }
