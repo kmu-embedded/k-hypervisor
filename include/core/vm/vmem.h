6dc1ef44f11c7c90e425349718109b18d0920972
iamwonseok <wonseok.ko@outlook.com>
2016-03-14 13:46:52 +0900

    [VM] Removed meaningless variables

--- include/core/vm/vmem.h
+++ include/core/vm/vmem.h
@@ -14,7 +14,7 @@ struct vmem {
 
 void vmem_setup();
 void vmem_create(struct vmem *vmem, vmid_t vmid);
-hvmm_status_t vmem_init(struct vmem *vmem, vmid_t vmid);
+hvmm_status_t vmem_init(struct vmem *vmem);
 hvmm_status_t vmem_save(void);
 hvmm_status_t vmem_restore(struct vmem *vmem);
 
