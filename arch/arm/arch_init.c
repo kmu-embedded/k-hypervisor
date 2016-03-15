8c39497213154f3e3eeb9a970449a9a589fd1d06
casionwoo <casionwoo@gmail.com>
2016-03-15 18:40:14 +0900

    Renamed vgic.[ch] as vgic_status.[ch] and removed unnecessary include

---
+++ arch/arm/arch_init.c
@@ -0,0 +1,21 @@
+#include "arch_init.h"
+#include "mm.h"
+
+extern uint32_t __hvc_vector;
+void SECTION(".init.arch") arch_cpu_init()
+{
+    uint32_t vector_base = (uint32_t) &__hvc_vector;
+    write_hvbar(vector_base);
+    assert(read_hvbar() == vector_base);
+    // TODO(wonseok): D-Cache invalidate and TLB flush.
+    // TODO(casionwoo): add cache operation in arch/arm
+}
+
+#include "gic-v2.h"
+void SECTION(".init.arch") arch_irq_init()
+{
+    // TODO(casionwoo): add a init function of irq handler table for hypervisor.
+    gic_init();
+
+    write_hcr(0x10 | 0x8);
+}
---
+++ arch/arm/vgic_status.h
@@ -0,0 +1,18 @@
+#ifndef __VGIC_H__
+#define __VGIC_H__
+
+#include <stdint.h>
+#include "gic-v2.h"
+
+struct vgic_status {
+    uint32_t lr[64];        /**< List Registers */
+    uint32_t hcr;           /**< Hypervisor Control Register */
+    uint32_t apr;           /**< Active Priorities Register */
+    uint32_t vmcr;          /**< Virtual Machine Control Register */
+};
+
+hvmm_status_t vgic_init_status(struct vgic_status *status);
+hvmm_status_t vgic_save_status(struct vgic_status *status);
+hvmm_status_t vgic_restore_status(struct vgic_status *status, vcpuid_t vcpuid);
+
+#endif
