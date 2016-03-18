b3e150ffc3cab36e9fd40064b5cf5d9747e5d49b
wonseok <magicyaba@gmail.com>
2016-03-18 18:28:11 +0900

    Refactored virq-related functions

--- include/core/vm/virq.h
+++ include/core/vm/virq.h
@@ -9,21 +9,17 @@
 #define MAX_PENDING_VIRQS    128
 #define MAX_NR_IRQ           1024
 
-struct virq_entry {
+struct lr_entry {
     uint32_t pirq;
     uint32_t virq;
     uint8_t hw;
     uint8_t valid;
 };
 
-struct virqmap_entry {
+struct virq_table {
     uint32_t enabled;
     uint32_t virq;
     uint32_t pirq;
-};
-
-struct guest_virqmap {
-    struct virqmap_entry map[MAX_NR_IRQ];
 };
 
 struct vgic_status {
@@ -35,8 +31,8 @@ struct vgic_status {
 
 struct virq {
     struct vgic_status vgic_status;
-    struct guest_virqmap *guest_virqmap;
-    struct virq_entry pending_irqs[MAX_PENDING_VIRQS +1];
+    struct virq_table map[MAX_NR_IRQ];
+    struct lr_entry pending_irqs[MAX_PENDING_VIRQS +1];
 };
 
 #define GUEST_IRQ_ENABLE 1
@@ -48,15 +44,15 @@ struct virq {
 void virq_create(struct virq *virq);
 void virq_init(struct virq *virq, vmid_t vmid);
 
-uint32_t pirq_to_virq(struct virq *v, uint32_t pirq);
+//uint32_t pirq_to_virq(struct virq *v, uint32_t pirq);
 uint32_t virq_to_pirq(struct virq *v, uint32_t virq);
 uint32_t pirq_to_enabled_virq(struct virq *v, uint32_t pirq);
 uint32_t virq_to_enabled_pirq(struct virq *v, uint32_t virq);
 
 void virq_enable(struct virq *v, uint32_t virq);
 
-hvmm_status_t virq_save(struct virq *virq);
-hvmm_status_t virq_restore(struct virq *virq, vmid_t vmid);
+void virq_save(struct virq *virq);
+void virq_restore(struct virq *virq, vmid_t vmid);
 
 bool is_guest_irq(uint32_t irq);
 
