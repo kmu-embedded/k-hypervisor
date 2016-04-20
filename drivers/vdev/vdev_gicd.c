e77e3fff9e8639e2a2af1a8cb5802f571623d749
casionwoo <casionwoo@gmail.com>
2016-04-20 15:11:16 +0900

    Added initialize routine of pdata of vdev_gicd

--- drivers/vdev/vdev_gicd.c
+++ drivers/vdev/vdev_gicd.c
@@ -42,6 +42,7 @@ int32_t vgicd_create_instance(void **pdata)
     struct vgicd *vgicd = malloc(sizeof(struct vgicd));
 
     memset(vgicd, 0, sizeof(struct vgicd));
+
     vgicd->typer = GICD_READ(GICD_TYPER);
     vgicd->iidr  = GICD_READ(GICD_IIDR);
     *pdata = vgicd;
