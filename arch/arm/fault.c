de5a0e3efbaa0d5253e826f08389fa7d21c5dd0f
iamwonseok <wonseok.ko@outlook.com>
2016-04-28 12:02:15 +0900

    Fixed get alignment fault address by valid registers

--- arch/arm/fault.c
+++ arch/arm/fault.c
@@ -8,12 +8,11 @@
 int handle_data_abort(struct core_regs *regs, uint32_t iss)
 {
     int ret = -1;
-    uint32_t fipa = read_cp32(HPFAR) << 8;
-    fipa |= (read_cp32(HDFAR) & PAGE_MASK);
-
+    uint32_t fipa = 0x0 ;
     switch (decode_fsc(iss)) {
         // TODO: remove unused cases.
         case FSR_TRANS_FAULT(1) ... FSR_TRANS_FAULT(3):
+            fipa = read_cp32(HPFAR) << 8;
             printf("Translation Fault!!\n");
             printf("\tWe do not allow translation fault from guest in stage 2 address\n");
             printf("\ttranslation, so if you see this message, you have to add a mapping\n");
@@ -55,6 +54,7 @@ int handle_data_abort(struct core_regs *regs, uint32_t iss)
             break;
 
         case FSR_ALINGMENT_FAULT: {
+            fipa |= (read_cp32(HDFAR) & PAGE_MASK);
             uint32_t hcr = read_cp32(HCR);
             printf("Alignment Fault!!\n");
             printf("ARM defines the root cause of alignment fault taken into hypervisor as belows\n");
@@ -73,7 +73,8 @@ int handle_data_abort(struct core_regs *regs, uint32_t iss)
             }
             printf("HCR: %x\n",  hcr);
             printf("HDCR %x\n", read_cp32(HDCR));
-            printf("fault address is here 0x%08x\n", fipa);
+            printf("HPFAR 0x%08x is UNK\n", read_cp32(HPFAR));
+            printf("HDFAR 0x%08x is available\n", fipa);
             break;
         }
         case FSR_DEBUG_EVENT:
