691722bea492a7fdb174d62d09d4a500b7800a49
casionwoo <casionwoo@gmail.com>
2017-03-28 13:32:00 +0900

    [kmus]: add vmcb_type_t to vm_config struct and config one as normal VM and the other as back-up VM

--- core/vm_config.c
+++ core/vm_config.c
@@ -3,6 +3,6 @@
 #include <size.h>
 
 struct vm_config vm_conf[] = {
-    { 1, SZ_256M, CFG_MEMMAP_GUEST0_ATAGS_OFFSET },
-    { 1, SZ_256M, CFG_MEMMAP_GUEST1_ATAGS_OFFSET }
+    { 1, SZ_256M, CFG_MEMMAP_GUEST0_ATAGS_OFFSET, VM_NORMAL},
+    { 1, SZ_256M, CFG_MEMMAP_GUEST1_ATAGS_OFFSET, VM_KMUS}
 };
