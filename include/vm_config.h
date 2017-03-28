a2f49985e750d81bca11fc4868d261bbe792fa09
casionwoo <casionwoo@gmail.com>
2017-03-28 13:32:00 +0900

    kmus: add vmcb_type_t to vm_config struct

--- include/vm_config.h
+++ include/vm_config.h
@@ -2,11 +2,13 @@
 #define __VM_CONFIG_H__
 
 #include <stdint.h>
+#include <core/vm/vm.h>
 
 struct vm_config {
     uint8_t nr_vcpus;
     uint32_t va_offsets;
     addr_t pa_start;
+    vmcb_type_t vmcb_type;
 };
 
 extern struct vm_config vm_conf[];
