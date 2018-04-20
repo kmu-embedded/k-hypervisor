19131f98323065109a75882b48dab8562650358f
jigi-kim <jigi.kim@gmail.com>
2018-04-20 15:53:07 +0900

    op-tee: add support for static shmem feature

--- arch/arm/smccc.c
+++ arch/arm/smccc.c
@@ -1,6 +1,7 @@
 #include <arch/armv7.h>
 #include <arch/psci.h>
 #include <arch/smccc.h>
+#include <core/scheduler.h>
 
 int emulate_arm_smccc(struct core_regs *regs)
 {
@@ -12,11 +13,24 @@ int emulate_arm_smccc(struct core_regs *regs)
         regs->gpr[0] = emulate_psci_cpu_on(regs);
         break;
 #endif
+    //TODO(jigi.kim): Add trusted OS related case, here.
     default:
         // Forward SMCCC as intended.
         arm_smccc_smc(regs->gpr[0], regs->gpr[1], regs->gpr[2], regs->gpr[3],
                       regs->gpr[4], regs->gpr[5], regs->gpr[6], regs->gpr[7],
                       (struct arm_smccc_res *) regs->gpr);
+
+        // Partition OP-TEE static shared memory space for each VM.
+        if ((function_id & 0x0000ffff) == 0x7) {
+            vcpuid_t vcpuid = get_current_vcpuid();
+
+            uint32_t base_addr = regs->gpr[1];
+            uint32_t offset = regs->gpr[2];
+
+            regs->gpr[1] = base_addr + (offset/CONFIG_NR_VMS) * vcpuid;
+            regs->gpr[2] = offset/CONFIG_NR_VMS;
+        }
+
     }
 
     return 0;
