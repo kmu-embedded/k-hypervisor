67ec74fcf7f5b8738b724da0c8f90bb0c9fb0dbe
jigi-kim <jigi.kim@gmail.com>
2018-07-04 07:50:15 +0900

    Modify smccc handler to identify smc function by service call range

--- arch/arm/smccc.c
+++ arch/arm/smccc.c
@@ -1,10 +1,40 @@
+#include <stdio.h>
 #include <arch/armv7.h>
 #include <arch/optee.h>
 #include <arch/psci.h>
 #include <arch/smccc.h>
-#include <core/scheduler.h>
+
+#define INVALID_SMCCC_FN -1
+
+int handle_misc_arm_smccc(struct core_regs *regs);
 
 int handle_arm_smccc(struct core_regs *regs)
+{
+    uint32_t function_id = regs->gpr[0];
+    int ret = 0;
+
+    switch(SMCCC_FN_SERVICE(function_id)) {
+    case SMCCC_SERVICE_ARCH:
+    case SMCCC_SERVICE_CPU:
+    case SMCCC_SERVICE_SIP:
+    case SMCCC_SERVICE_OEM:
+    case SMCCC_SERVICE_STANDARD:
+    case SMCCC_SERVICE_HYPERVISOR:
+        printf("No corresponding smc handler for fn 0x%08x\n", function_id);
+        break;
+    case SMCCC_SERVICE_TRUSTED_APP ... SMCCC_SERVICE_TRUSTED_APP_END:
+    case SMCCC_SERVICE_TRUSTED_OS ... SMCCC_SERVICE_TRUSTED_OS_END:
+        printf("fn: 0x%08x\n", function_id);
+        handle_optee_smc(regs);
+        break;
+    default:
+        ret = handle_misc_arm_smccc(regs);
+    }
+
+    return ret;
+}
+
+int handle_misc_arm_smccc(struct core_regs *regs)
 {
     uint32_t function_id = regs->gpr[0];
 
@@ -14,9 +44,9 @@ int handle_arm_smccc(struct core_regs *regs)
         regs->gpr[0] = emulate_psci_cpu_on(regs);
         break;
 #endif
-    //TODO: Add trusted OS related case, here.
     default:
-        handle_optee_smc(regs->gpr);
+        printf("No corresponding smc handler for fn 0x%08x\n", function_id);
+        return -1;
     }
 
     return 0;
--- include/arch/smccc.h
+++ include/arch/smccc.h
@@ -3,6 +3,29 @@
 
 #include <arch/armv7.h>
 
+#define SMCCC_FN_CALL_TYPE(fn)  (((fn) & 0x80000000) >> 31)
+#define SMCCC_FN_CONV(fn)       (((fn) & 0x40000000) >> 30)
+#define SMCCC_FN_SERVICE(fn)    (((fn) & 0x3f000000) >> 24)
+#define SMCCC_FN_NUM(fn)        ((fn) & 0x0000FFFF)
+
+enum smccc_fn_service_range {
+    SMCCC_SERVICE_ARCH = 0,
+    SMCCC_SERVICE_CPU,
+    SMCCC_SERVICE_SIP,
+    SMCCC_SERVICE_OEM,
+    SMCCC_SERVICE_STANDARD,
+    SMCCC_SERVICE_HYPERVISOR,
+
+    // SMCCC_SERVICE_RESERVED = 7,
+    // SMCCC_SERVICE_RESERVED_END = 47,
+
+    SMCCC_SERVICE_TRUSTED_APP = 48,
+    SMCCC_SERVICE_TRUSTED_APP_END = 49,
+
+    SMCCC_SERVICE_TRUSTED_OS = 50,
+    SMCCC_SERVICE_TRUSTED_OS_END = 63
+};
+
 struct arm_smccc_res {
     unsigned long a0;
     unsigned long a1;
